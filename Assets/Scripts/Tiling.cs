add1a9098a7854cd863234dc6f4c8316bdba738c
Joseph <joseph.gangestad@gmail.com>
2017-10-01 05:34:01 -0400

    Initial commit

---
+++ Assets/Scripts/Tiling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+
+public class Tiling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
